10129c0800777212ed297999f4807bbf98edbed1
Irina Mogozova <12902@coderacademy.edu.au>
2023-01-17 20:51:37 +1100

    Create Product Schema and Model

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ app.use("/carts", cartRouter)
 
 app.listen(PORT, () => {
     console.log("Server Started")
+    mongoose.set('strictQuery', false)
     mongoose.connect("mongodb://127.0.0.1:27017/ecommerce", () => {
         console.log("Database Connected")
     })
